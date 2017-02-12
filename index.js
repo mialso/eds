9734801b236c0933c0d54f8f1bd4037950bbcbf4
Mikhail Sotnikov <mialsot@gmail.com>
2017-02-12 22:28:35 +0300

    browser client initialized

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const serve = require('koa-static')
+//const mount = require('koa-mount')
+const fs = require('fs')
 const koa = require('koa')
 
 const port = 5999
@@ -8,6 +10,16 @@ const port = 5999
 const app = koa()
 
 app.use(serve('public'))
+const data = fs.readFileSync('public/index.html')
+
+app.use(function * (next) {
+	yield next
+	//console.log(JSON.stringify(this))
+	if (this.url.split('.').pop() !== 'js') {
+		console.log('index.html written')
+		this.body = data.toString()
+	}
+})
 
 app.listen(port)
  
